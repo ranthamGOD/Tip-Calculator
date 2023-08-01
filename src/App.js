049dbf7b5b2c17fdbacfac89c692e124bcc97224
Akash patil <ap4180530@gmail.com>
2023-08-01 12:17:28 +0530

    completed Percentage component

--- src/App.js
+++ src/App.js
@@ -6,4 +6,41 @@ export default function App() {
   );
 }
 
-function TipCalculator() {}
+function TipCalculator() {
+  return (
+    <div>
+      <BillInput> How much was the bill?</BillInput>
+      <Percentage>How did you like the service? </Percentage>
+      <Percentage> </Percentage>
+      <Output />
+      <Reset />
+    </div>
+  );
+}
+
+function BillInput({ children }) {
+  return (
+    <div>
+      {children}
+      <input type="text" name="bill" placeholder="Bill" />
+    </div>
+  );
+}
+
+function Percentage({ children }) {
+  return (
+    <div>
+      {children}
+      <select>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">Dissatisfied (5%)</option>
+        <option value="10">Dissatisfied (10%)</option>
+        <option value="20">Dissatisfied (20%)</option>
+      </select>
+    </div>
+  );
+}
+
+function Output() {}
+
+function Reset() {}
