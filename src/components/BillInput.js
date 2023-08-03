57a0b3bf53df7cb33dfd7cfa3a5f70bb7a8d0574
Akash patil <ap4180530@gmail.com>
2023-08-03 11:29:28 +0530

    Install materibal ui to our project

--- src/components/BillInput.js
+++ src/components/BillInput.js
@@ -2,6 +2,7 @@ export function BillInput({ bill, onSetBill }) {
   return (
     <div>
       <label>How much was the bill? </label>
+
       <input
         type="text"
         placeholder="Bill Value"
