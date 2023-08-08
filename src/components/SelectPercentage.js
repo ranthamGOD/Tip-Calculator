e2a57a48095bd9b68081dc51a465ac3c22351ae4
Akash patil <ap4180530@gmail.com>
2023-08-08 18:47:55 +0530

    changing filter value name

--- src/components/SelectPercentage.js
+++ src/components/SelectPercentage.js
@@ -9,7 +9,7 @@ export function SelectPercentage({ children, Percentage, onSelect }) {
         value={Percentage}
         onChange={(e) => onSelect(Number(e.target.value))}
       >
-        <option value="0">dissatisfied (0%)</option>
+        <option value="0">Dissatisfied (0%)</option>
         <option value="5">Dissatisfied (5%)</option>
         <option value="10">Dissatisfied (10%)</option>
         <option value="20">Dissatisfied (20%)</option>
